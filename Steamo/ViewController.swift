89f2fa049dc7661810cbe3f98524117956d7676c
Max Kraev <max.kraev@gmail.com>
2019-11-20 19:21:50 +0300

    Initial Commit

---
+++ Steamo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Steamo
+//
+//  Created by Max Kraev on 20.11.2019.
+//  Copyright © 2019 Max Kraev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
