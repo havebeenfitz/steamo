7c1d69f0ad93c1d2609b6f196c651475bf560022
Max Kraev <max.kraev@gmail.com>
2019-12-07 23:10:01 +0300

    reset realm scheme

--- Steamo/App/AppDelegate.swift
+++ Steamo/App/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let container = DependencyContainer()
         
         // Миграция для реалма, если нужна
-        container.databaseManager.migrate(with: 4)
+        container.databaseManager.migrate(with: 0)
 
         if #available(iOS 13.0, *) {} else {
             window = UIWindow(frame: UIScreen.main.bounds)
